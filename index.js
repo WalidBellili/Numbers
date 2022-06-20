3f726d65e4fb3a025e390a5e57b83820395d60fb
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-06-20 16:25:59 +0200

    number exo 5é

--- index.js
+++ index.js
@@ -17,13 +17,24 @@
 // console.log(rounded);
 
 // EXO 4
-const test = 12
-const bis = 5
+// const test = 12
+// const bis = 5
 
-console.log(test + bis);
-console.log(test - bis);
-console.log(test / bis);
-console.log(test * bis);
-console.log(test % bis);
-console.log(test ** bis);
+// console.log(test + bis);
+// console.log(test - bis);
+// console.log(test / bis);
+// console.log(test * bis);
+// console.log(test % bis);
+// console.log(test ** bis);
+//  exo 5
 
+// let modulo = 999 %1; OUI
+// console.log(modulo);
+// let modulo = 999 %2
+// console.log(modulo); non
+// let modulo = 999 %3
+// console.log(modulo); OUI
+// let modulo = 999 %4
+// console.log(modulo); OUI 3 et pas 0
+// let modulo = 999 %5
+// console.log(modulo); non
