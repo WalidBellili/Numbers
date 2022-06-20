4711e3137887172ca2cac11505a468b4638d2e7f
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-06-20 16:38:31 +0200

    exo number terminé

--- index.js
+++ index.js
@@ -26,9 +26,11 @@
 // console.log(test * bis);
 // console.log(test % bis);
 // console.log(test ** bis);
+
+
 //  exo 5
 
-// let modulo = 999 %1; OUI
+// let modulo = 999 %1;OUI
 // console.log(modulo);
 // let modulo = 999 %2
 // console.log(modulo); non
@@ -38,3 +40,47 @@
 // console.log(modulo); OUI 3 et pas 0
 // let modulo = 999 %5
 // console.log(modulo); non
+// *******************************************
+// let modulo = 25 %1; oui
+// console.log(modulo);
+// let modulo = 25 %2
+// console.log(modulo); non
+// let modulo = 25 %3
+// console.log(modulo); non
+// let modulo = 25 % 4
+// console.log(modulo); non 1
+// let modulo = 25 %5
+// console.log(modulo); Oui
+// *******************************************
+// let modulo = 190280 %1; oui 0
+// console.log(modulo);
+// let modulo = 190280  %2
+// console.log(modulo); Oui 0
+// let modulo = 190280  %3
+// console.log(modulo); pair par 2
+// let modulo = 190280  % 4
+// console.log(modulo); oui 0
+// let modulo = 190280  %5
+// console.log(modulo); OUI
+// *******************************************
+// let modulo = 47%1; oui 
+// console.log(modulo);
+// let modulo = 47 %2
+// console.log(modulo); non 1
+// let modulo = 47 %3
+// console.log(modulo); non 2
+// let modulo = 47 % 4
+// console.log(modulo); non 3
+// let modulo = 47 %5
+// console.log(modulo); non 2
+// *******************************************
+// let modulo = 691%1;
+// console.log(modulo); oui
+// let modulo = 691 %2
+// console.log(modulo); non 1
+// let modulo = 691 %3
+// console.log(modulo); non 1
+// let modulo = 691 % 4
+// console.log(modulo); non 3
+// let modulo = 691 %5
+// console.log(modulo); non 1
