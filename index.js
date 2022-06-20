fc9ea01b7198079b0e85ec1fb5125f72a7ed6b8b
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-06-20 16:18:32 +0200

    numbers

---
+++ index.js
@@ -0,0 +1,21 @@
+// EXO 1
+
+// const integer = 102;
+// const float = 13.9
+// console.log(integer, float);
+
+// EXO 2
+
+// const basic = 34
+// const stingify = basic.toString()
+// console.log(stingify);
+
+// EXO 3
+
+// const num = 1.5
+// const rounded = Math.round(num)
+// console.log(rounded);
+
+// EXO 4
+const test = 12
+const bis = 5
